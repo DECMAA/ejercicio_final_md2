4ca66b8be513931e288514a693dd3adc14bad90c
Denisse Maturana <denisse.maturana@gmail.com>
2026-01-02 23:46:40 -0300

    creacion de ciudades y datos temperatura

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const ciudades = [
+    { id: 1, name: "Buenos Aires, Argentina", temp: 20, estado: "Nublado", icon: "cloud.png" },
+    { id: 2, name: "Sucre, Bolivia", temp: 19, estado: "Lluvia", icon: "rain.png" },
+    { id: 3, name: "Brasilia,Brasil", temp: 20, estado: "Parcial", icon: "sun.png" },
+    { id: 4, name: "Santiago,Chile", temp: 15, estado: "Lluvia", icon: "rain.png" },
+    { id: 5, name: "Bogotá,Colombia", temp: 30, estado: "Tormenta", icon: "storm.png" },
+    { id: 6, name:"Quito, Ecuador", temp: 25, estado: "Nublado", icon: "cloud.png" },
+    { id: 7, name:"Georgetown, Guyana", temp: 29, estado: "Soleado", icon: "sun.png" },
+    { id: 8, name:"Paramaribo, Surinam ", temp: 18, estado: "Llovizna", icon: "drizzle.png" },
+    { id: 9, name:"Lima, Perú", temp: 8, estado: "Nieve", icon: "snow.png" },
+    { id: 10, name: "Asunción, Paraguay", temp: 28, estado: "Parcial", icon: "sun.png" }
+    { id: 11, name: "Montevideo, Uruguay)", temp: 25, estado: "Parcial", icon: "sun.png" }
+    { id: 12, name: "Caracas, Venezuela", temp: 26, estado: "Parcial", icon: "sun.png" }
+];
+
+const semana = [
+    {dia: "Lunes", temp:22 },
+    {dia: "Martes", temp:20 },
+    {dia: "Miercoles", temp:22 },
+    {dia: "Jueves", temp:32 },
+    {dia: "Viernes", temp:18 },
+    {dia: "Sabado", temp:22 },
+    {dia: "Domingo", temp:12 },
+
+]
+
